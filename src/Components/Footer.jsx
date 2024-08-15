ec2b28bc67c9c24e09e3a13a42ae70073aa2b1a9
Mazed1413 <mamazed1314@gmail.com>
2024-08-15 22:01:34 +0600

    add footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,67 @@
+import { FaGooglePlay } from "react-icons/fa6";
+
+const Footer = () => {
+  return (
+    <footer class="bg-yellow-400 py-8 text-gray-700">
+      <div class="px-2 md:px-20">
+        <div class="flex flex-col md:flex-row justify-between border-b-2 border-dashed border-gray-700 pb-8">
+          <div className="mx-auto px-2 md:px-6">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+              <div className="mb-8">
+                <h4 className="text-lg font-semibold mb-4 text-center">
+                  About Us
+                </h4>
+                <p className="text-sm text-center">
+                  Radiant is a premier online product showcase platform
+                  dedicated to gathering valuable insights through user-friendly
+                  and efficient web application.
+                </p>
+              </div>
+              <div className="mb-8 text-center">
+                <h4 className="text-lg font-semibold mb-4 text-center">
+                  Contact Us
+                </h4>
+                <p className="text-sm">
+                  2312 GSE Street, Chittagong, Bangladesh
+                </p>
+                <p className="text-sm">Email: radiant@gmail.com</p>
+                <p className="text-sm">Phone: +123-456-7890</p>
+              </div>
+              <div className="mb-8 ">
+                <h3 class="text-xl font-bold p-4">Download our app</h3>
+
+                <button class="border border-gray-700 rounded-lg flex justify-center">
+                  <a href="#" class="mt-3 p-2">
+                    <FaGooglePlay className="text-2xl" />
+                  </a>
+                  <div class="m-2 text-left">
+                    <p class="font-semibold">GET IT ON</p>
+                    <h3 class="text-2xl font-bold">Google Play</h3>
+                  </div>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="flex flex-col gap-5 md:flex-row justify-between mt-5">
+          <div>
+            <p>&copy;all rights reserved, Radiant.{new Date().getFullYear()}</p>
+          </div>
+
+          <div>
+            <a class="mr-3" href="#">
+              Terms & Condition
+            </a>
+            <a class="mr-3" href="#">
+              Return & refund policy
+            </a>
+            <a href="#">Privacy policy</a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
 
 const Layout = () => {
   return (
@@ -8,6 +9,7 @@ const Layout = () => {
       <div className="min-h-screen mt-20 px-4 md:px-0 md:w-11/12 mx-auto">
         <Outlet></Outlet>
       </div>
+      <Footer></Footer>
     </div>
   );
 };
